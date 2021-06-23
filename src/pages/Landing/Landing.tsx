6fb1d75f08eed63d43a3d812461c88de6b99b5cb
danvb112 <danielbonasser@gmail.com>
2021-06-23 11:01:57 -0300

    Add LandingPage

---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import './styles.css'
+
+import LandingImage from '../../assets/images/logo.png'
+
+function Landing() {
+
+    const nameProject = "Winest"
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <h1>{nameProject}</h1>
+                    <h2>Your Digital Cellar Plataform</h2>
+                </div>
+
+                <img
+                    src={LandingImage}
+                    alt="Landing-image"
+                    className='hero-image'
+                />
+
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
+
